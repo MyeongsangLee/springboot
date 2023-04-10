078b7554a07f176031ec4faaadd12c42c1352631
Lee Myeongsang <mslee0052@gmail.com>
2023-04-11 05:36:49 +0900

    commit

---
+++ memory-v2/src/main/java/memory/Memory.java
@@ -0,0 +1,15 @@
+package memory;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter @Setter
+@ToString
+public class Memory {
+
+    private long used;
+    private long max;
+}
